07bef44b3af5bed5fd336dc8e00a2f4a44bf4d44
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-02-14 14:45:02 +0100

    Add OrderForm

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+//import styles from './OrderForm.scss';
+import {Row, Col} from 'react-flexbox-grid';
+import OrderSummary from '../OrderSummary/OrderSummary';
+
+const OrderForm = () => {
+
+  <Row>
+    <Col xs={12}>
+      <OrderSummary />
+    </Col>
+  </Row>;
+};
+
+export default OrderForm;
