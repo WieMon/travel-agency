22e7a6bcc00398dec659bbb85dd0e2f6150ebdc4
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-02-15 15:37:59 +0100

    Add OrderOption

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,10 +3,18 @@ import PropTypes from 'prop-types';
 //import styles from './OrderForm.scss';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
 const OrderForm = (props) => (
 
   <Row>
+    {pricing.map(option =>
+      <Col md={4} key={option.id}>
+        <OrderOption {...option} />
+        
+      </Col>
+    )}
     <Col xs={12}>
       <OrderSummary cost={props.tripCost} options={props.options}/>
     </Col>
