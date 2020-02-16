94f9ead61b27175eda0c197da6ee4629165c8618
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-02-16 14:07:26 +0100

    Add OrderOptionNumber

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,10 +1,29 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+//import { ProgressPlugin } from 'webpack';
+import PropTypes from 'prop-types';
+import {formatPrice} from '../../../utils/formatPrice';
 
-const OrderOptionNumber =() => (
-  <div>
-   OrderOptionNumber
+const OrderOptionNumber =(currentValue, limits, setOptionValue, price) => (
+  <div className={styles.number}>
+    <input
+      type='number'
+      className={styles.inputSmall}
+      value={currentValue}
+      min={limits.min}
+      max={limits.max}
+      onChange={event => setOptionValue(event.currentTarget.value)}
+    >
+      {formatPrice(price)}
+    </input>
   </div>
 );
+
+OrderOptionNumber.propTypes = {
+  currentValue: PropTypes.number,
+  limits: PropTypes.object,
+  setOptionValue: PropTypes.func,
+  price: PropTypes.node,
+};
 
 export default OrderOptionNumber;
