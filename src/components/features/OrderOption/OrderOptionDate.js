87e4313c3b123e15239e1dfc95e629ebec6676f6
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-02-28 17:05:12 +0100

    Fix OrderOptionDate

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import DatePicker from 'react-datepicker';
 import PropTypes from 'prop-types';
 import styles from './OrderOption.scss';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
-class OrderOptionDate extends React.Component {
+const OrderOptionDate = () => {
 
-  render() {
-    const {currentValue, setOptionValue} = this.props;
+  const [currentValue, setOptionValue] = useState(new Date());
 
-    return (
-      <DatePicker
-        className={styles.date}
-        selected={currentValue}
-        onChange={setOptionValue}
-      />
-    );
-  }
-}
+  return (
+    <DatePicker
+      className={styles.input}
+      selected={currentValue}
+      onChange={date => setOptionValue(date)}
+    />
+  );
+};
 
 OrderOptionDate.propTypes = {
   currentValue: PropTypes.string,
